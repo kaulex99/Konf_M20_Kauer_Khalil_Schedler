4a214ccb2d0be8b209f6c322dde7f5d16a7372f1
Fabian Schedler <dev.fabian.schedler@gmail.com>
2021-03-11 14:45:22 +0100

    added class FabiDrink with first implementation

---
+++ src/main/java/at/fhj/iit/FabiDrink.java
@@ -0,0 +1,63 @@
+package at.fhj.iit;
+
+import javax.crypto.AEADBadTagException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * FabiDrink
+ * Represents a wild mix of liquids
+ */
+public class FabiDrink extends Drink {
+
+    protected ArrayList<Liquid> liquids = new ArrayList<>();
+    /**
+     * Creates a FabiDrink object with given name and a main liquid
+     *
+     * @param name name of drink
+     * @param mainLiquid    the main liquid in the drink
+     */
+    FabiDrink(String name, Liquid mainLiquid) {
+        super(name);
+        this.liquids.add(mainLiquid);
+    }
+
+    FabiDrink(String name, Liquid ...liquids){
+        super(name);
+        this.liquids.addAll(Arrays.asList(liquids));
+    }
+
+    void addLiquid(Liquid l){
+        this.liquids.add(l);
+    }
+
+    @Override
+    public double getVolume() {
+        double v = 0;
+
+        for(Liquid l : liquids){
+            v += l.getVolume();
+        }
+        return v;
+    }
+
+    @Override
+    public double getAlcoholPercent() {
+        double alcQuantity = 0;
+        for(Liquid l : liquids){
+            alcQuantity += l.getVolume() * l.getAlcoholPercent();
+        }
+        return alcQuantity * this.getVolume();
+    }
+
+    @Override
+    public boolean isAlcoholic() {
+        for(Liquid l : liquids){
+           if(l.getAlcoholPercent() > 0){
+               return true;
+           }
+        }
+        return false;
+    }
+}
