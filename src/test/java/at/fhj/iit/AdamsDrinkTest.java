344e5de10bf981a050d9a7bc322cdc83b7c44a7b
Adam Khalil <adam.khalil@edu.fh-joanneum.at>
2021-04-24 13:34:14 +0200

    New Junit test for AdamsDrink class added

---
+++ src/test/java/at/fhj/iit/AdamsDrinkTest.java
@@ -0,0 +1,58 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Adam Khalil
+ * @version 1.0
+ * @see AdamsDrink
+ */
+
+class AdamsDrinkTest {
+
+    private AdamsDrink drink;
+
+    @BeforeEach
+    void setUp() {
+        Liquid liquid = new Liquid("beverage", 6, 10);
+        drink = new AdamsDrink("drinkName", liquid);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        drink = null;
+        //add other teardown logic if necessary
+
+    }
+
+    @Test
+    @DisplayName("Check if the alcohol percentage calculation works correctly")
+    void AlcoholPercent() {
+        assertEquals(4, drink.getAlcoholPercent());
+    }
+
+    @Test
+    @DisplayName("Verify that the to string only returns the name")
+    void testToString() {
+        assertEquals("drinkName", drink.toString());
+    }
+
+    @Test
+    @DisplayName("Check if one gets added to the volume")
+    void getVolume() {
+        assertEquals(7, drink.getVolume());
+    }
+
+    @Test
+    @DisplayName("Check if the drink is alcoholic")
+    void isAlcoholic() {
+        assertTrue(drink.isAlcoholic());
+    }
+
+}
