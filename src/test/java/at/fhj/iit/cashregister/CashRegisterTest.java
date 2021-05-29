86ca188c99a8f1130e3890677c2abbba3811c47d
Fabian Schedler <dev.fabian.schedler@gmail.com>
2021-05-29 19:27:38 +0200

    initial test setup for cash register feature

---
+++ src/test/java/at/fhj/iit/cashregister/CashRegisterTest.java
@@ -0,0 +1,51 @@
+package at.fhj.iit.cashregister;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashRegisterTest {
+
+    private CashRegister cashRegister;
+
+    @BeforeEach
+    void setUp() {
+        cashRegister = new CashRegister();
+    }
+
+    @Test
+    void sell() {
+        // TODO @Fabian implement
+    }
+
+    @Test
+    void getNoneAlcoholicRevenue() {
+        // TODO @Fabian implement
+    }
+
+    @Test
+    void getLowAlcoholicRevenue() {
+        // TODO @Adam implement
+    }
+
+    @Test
+    void getHighAlcoholicRevenue() {
+        // TODO @Adam implement
+    }
+
+    @Test
+    void getDayRevenue() {
+        // TODO @Alex implement
+    }
+
+    @Test
+    void getPersonRevenue() {
+        // TODO @Alex implement
+    }
+
+    @Test
+    void getPersonDayRevenue() {
+        // TODO @Alex implement
+    }
+}
